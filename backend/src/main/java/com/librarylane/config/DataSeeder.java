3512061415497dca2465a3c6c4808497b2f9c55f
heathergauthier2018 <heathergauthier18@gmail.com>
2026-09-06 23:56:11 -0500

    chore: separate environment-specific configuration (#4)

--- backend/src/main/java/com/librarylane/config/DataSeeder.java
+++ backend/src/main/java/com/librarylane/config/DataSeeder.java
@@ -7,8 +7,10 @@ import com.librarylane.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class DataSeeder {
 
     @Bean
